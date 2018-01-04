6997c3907d4fb64986c708fc26368290053c7b05
Adem Sahin <sahinad@gmail.com>
2018-01-04 23:43:40 +0100

    HTTP Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,8 @@ import { ContactComponent } from './contact/contact.component';
 import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
+import { ProcessHttpmsgService } from './services/process-httpmsg.service';
+import { baseURL } from './shared/baseurl';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { LoginComponent } from './login/login.component';
@@ -49,7 +51,13 @@ import { LoginComponent } from './login/login.component';
   entryComponents: [
     LoginComponent
   ],
-  providers: [DishService, PromotionService, LeaderService],
+  providers: [
+    DishService,
+    PromotionService,
+    LeaderService,
+    {provide: 'BaseURL', useValue: baseURL},
+    ProcessHttpmsgService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -1,30 +1,39 @@
 import { Injectable } from '@angular/core';
 import { Dish } from '../shared/dish';
-import { DISHES } from '../shared/dishes';
+import { Http, Response } from '@angular/http';
+import { baseURL } from '../shared/baseurl';
+import { ProcessHttpmsgService } from './process-httpmsg.service';
 
 import { Observable } from 'rxjs/Observable';
 
 import 'rxjs/add/operator/delay';
 import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class DishService {
 
-  constructor() { }
+  constructor(private http: Http,
+    private processHTTPMsgService: ProcessHttpmsgService) { }
 
-  getDishes(): Observable<Dish[]> {
-    return Observable.of(DISHES).delay(2000);
-  }
+    getDishes(): Observable<Dish[]> {
+      return this.http.get(baseURL + 'dishes')
+                      .map(res => this.processHTTPMsgService.extractData(res));
+    }
 
-  getDish(id: number): Observable<Dish> {
-    return Observable.of(DISHES.filter((dish) => (dish.id === id))[0]).delay(2000);
-  }
+    getDish(id: number): Observable<Dish> {
+      return  this.http.get(baseURL + 'dishes/' + id)
+                      .map(res => this.processHTTPMsgService.extractData(res));
+    }
 
-  getFeaturedDish(): Observable<Dish> {
-    return Observable.of(DISHES.filter((dish) => dish.featured)[0]).delay(2000);
-  }
+    getFeaturedDish(): Observable<Dish> {
+      return this.http.get(baseURL + 'dishes?featured=true')
+                      .map(res => this.processHTTPMsgService.extractData(res)[0]);
+    }
 
-  getDishIds(): Observable<number[]> {
-    return Observable.of(DISHES.map(dish => dish.id));
-  }
+    getDishIds(): Observable<number[]> {
+      return this.getDishes()
+        .map(dishes => dishes.map(dish => dish.id));
+    }
 }
