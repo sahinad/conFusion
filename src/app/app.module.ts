a4b4be766c42bbd1e615125bfdcf05f1c7d265fb
Adem Sahin <sahinad@gmail.com>
2018-01-05 21:02:34 +0100

    Directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,6 +27,7 @@ import { RestangularConfigFactory } from './shared/restConfig';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { LoginComponent } from './login/login.component';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
@@ -38,7 +39,8 @@ import { LoginComponent } from './login/login.component';
     HomeComponent,
     AboutComponent,
     ContactComponent,
-    LoginComponent
+    LoginComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
